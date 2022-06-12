cb9a3b0d35d8f5d070b4f71a054f0c1ace1396ea
NyxHexen <jd.davis948@gmail.com>
2022-06-12 15:05:32 +0100

    Replaced all single quotes with double quotes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // Both functions below add *-open class to the body on click
 // Nav Toggle Button
-const navToggle = document.querySelector('.nav--toggle');
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+const navToggle = document.querySelector(".nav--toggle");
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
 });
 // Form Toggle Button
-const signupToggle = document.querySelector('.signup-form--toggle');
+const signupToggle = document.querySelector(".signup-form--toggle");
 if (signupToggle) {
-    signupToggle.addEventListener('click', () => {
-        document.body.classList.toggle('form-open');
+    signupToggle.addEventListener("click", () => {
+        document.body.classList.toggle("form-open");
     });
-};
+}
